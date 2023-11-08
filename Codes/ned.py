e34dbbc933054f89fc47c80f550bdb62fbe4ea01
hossein4527 <hossein.khaksar@hotmail.com>
2023-11-08 19:01:11 +0330

    No Regime- 2Reg - 3Reg has been updated

--- Codes/ned.py
+++ Codes/ned.py
@@ -174,8 +174,222 @@ class VG_Modules_Model1(object):
         MAP=np.mean(np.abs((selected_data - forcast_ts)))
         return RMSE, NRMSE, MAPE, MAP
     
-
 class VG_Modules_Model2(object):
+    def __init__(self):
+        self.bw = 1
+    
+    #Usual Model Two Prediction functions
+    def node_similarity(self, time_series, T=20, tp=50):
+        G = visibility_graph(time_series)
+        adj_matrix = np.array(adjacency_matrix(G).todense())
+        E = G.number_of_edges()
+        k = adj_matrix.sum(1)
+        P = adj_matrix/k
+        n = k.shape[0]
+        s_srw = np.zeros((n,n))
+        pi = np.diag(np.ones(n))
+        for t in range(T):
+            pi = P@pi
+            s_srw += (k*pi+k.T*pi.T)/(2*E)
+        if tp != 0:
+            triup = np.triu(s_srw,k=-tp)
+            triup[triup != 0] = 1
+            s_srw = np.tril(s_srw,k=-1) * triup
+        return np.argmax(s_srw,axis=1) , s_srw
+    
+    def ts_forcast_model2_weighted(self,time_serie, tp=30):
+        arg , s_srw = self.node_similarity(time_serie,T=20,tp=30)
+        sum_s = s_srw.sum(axis=1)
+        sum_s[sum_s == 0] = np.inf
+        ws_m = (s_srw.T/sum_s).T
+        ws = np.zeros((ws_m.shape[0], tp))
+        for i in range(ws_m.shape[0]):
+            ws[i] = np.roll(ws_m[i], tp-i)[:tp]
+        start_point = 31
+        forcast_ts = np.zeros_like(time_serie)
+        forcast_ts[:start_point] = time_serie[:start_point]
+        for i in range(start_point-1,time_serie.shape[0]-1):
+            y = time_serie[i]+(time_serie[i]-time_serie[i-tp:i])/(i-np.arange(i-tp,i,1))
+            forcast_ts[i+1]=(ws[i]*y).sum()
+        
+        return forcast_ts
+    
+    #Prediction Model Two Based on 2 Regimed Data
+    def most_similarity_matrix(self,time_series, T=20, tp=50):
+        G = visibility_graph(time_series)
+        adj_matrix = np.array(adjacency_matrix(G).todense())
+        E = G.number_of_edges()
+        k = adj_matrix.sum(1)
+        P = adj_matrix/k
+        n = k.shape[0]
+        s_srw = np.zeros((n,n))
+        pi = np.diag(np.ones(n))
+        for t in range(T):
+            pi = P@pi
+            s_srw += (k*pi+k.T*pi.T)/(2*E)
+        if tp != 0:
+            triup = np.triu(s_srw,k=-tp)
+            triup[triup != 0] = 1
+            s_srw = np.tril(s_srw,k=-1) * triup
+        return s_srw
+    
+    def selective_msn_2Regimed_Ox_model2_weighted(self,srw,df,  tp=20):
+        indx = []
+        rg_3 = []
+        ms_reg = []
+        for k in range(len(srw)):
+            indx.append([])
+            rg_3.append([])
+            rg_3[-1].append(df.iloc[k]['Ox_2'])
+            for i in range(1,tp):
+                indx[-1].append(np.where(srw[k] == np.sort(srw[k])[-i])[0][0])
+                rg_3[-1].append(df.iloc[indx[-1][-1]]['Ox_2'])
+            try:
+                ms_reg.append(indx[-1][np.where(rg_3[-1]==rg_3[-1][0])[0][1]])
+            except IndexError:
+                ms_reg.append(indx[-1][1])
+        return ms_reg
+    
+    def ts_forcast_2Regimed_model2_weighted(self, time_serie, df):
+        start_point = 31
+        forcast_ts = np.zeros_like(time_serie)
+        forcast_ts[:start_point] = time_serie[:start_point]
+        msns = self.most_similarity_matrix(time_serie,T=20,tp=50)
+        msn = self.selective_msn_2Regimed_Ox_model2_weighted(msns, df)
+        for i in range(start_point-1,time_serie.shape[0]-1):
+            dy = time_serie[i]-time_serie[msn[i]]
+            y = time_serie[i]+dy/(i-msn[i])
+            yhat = (i-msn[i])/(i-msn[i]+1)*y+(1/(i-msn[i]+1))*time_serie[i]
+            forcast_ts[i+1]=yhat
+        return forcast_ts
+    
+    #Prediction Model Two Based on 3 Regimed Data
+    def most_similarity_matrix(self,time_series, T=20, tp=50):
+        G = visibility_graph(time_series)
+        adj_matrix = np.array(adjacency_matrix(G).todense())
+        E = G.number_of_edges()
+        k = adj_matrix.sum(1)
+        P = adj_matrix/k
+        n = k.shape[0]
+        s_srw = np.zeros((n,n))
+        pi = np.diag(np.ones(n))
+        for t in range(T):
+            pi = P@pi
+            s_srw += (k*pi+k.T*pi.T)/(2*E)
+        if tp != 0:
+            triup = np.triu(s_srw,k=-tp)
+            triup[triup != 0] = 1
+            s_srw = np.tril(s_srw,k=-1) * triup
+        return s_srw
+    
+    def selective_msn_3Regimed_Ox_model2_weighted(self,srw,df,  tp=20):
+        indx = []
+        rg_3 = []
+        ms_reg = []
+        for k in range(len(srw)):
+            indx.append([])
+            rg_3.append([])
+            rg_3[-1].append(df.iloc[k]['Reg_3'])
+            for i in range(1,tp):
+                indx[-1].append(np.where(srw[k] == np.sort(srw[k])[-i])[0][0])
+                rg_3[-1].append(df.iloc[indx[-1][-1]]['Reg_3'])
+            try:
+                ms_reg.append(indx[-1][np.where(rg_3[-1]==rg_3[-1][0])[0][1]])
+            except IndexError:
+                ms_reg.append(indx[-1][1])
+        return ms_reg
+    
+    def ts_forcast_3Regimed_model2_weighted(self, time_serie, df):
+        start_point = 31
+        forcast_ts = np.zeros_like(time_serie)
+        forcast_ts[:start_point] = time_serie[:start_point]
+        msns = self.most_similarity_matrix(time_serie,T=20,tp=50)
+        msn = self.selective_msn_3Regimed_Ox_model2_weighted(msns, df)
+        for i in range(start_point-1,time_serie.shape[0]-1):
+            dy = time_serie[i]-time_serie[msn[i]]
+            y = time_serie[i]+dy/(i-msn[i])
+            yhat = (i-msn[i])/(i-msn[i]+1)*y+(1/(i-msn[i]+1))*time_serie[i]
+            forcast_ts[i+1]=yhat
+        return forcast_ts
+    
+    def ts_forcast_3Regimed_weighted_compact(self, df , time_serie):
+        s_srw = self.most_similarity_matrix(time_serie, T=20,tp=50)
+        sum_s = s_srw.sum(axis=1)
+        sum_s[sum_s == 0] = np.inf
+        ws_m = (s_srw.T/sum_s).T
+        ws = np.zeros((ws_m.shape[0], tp))
+        
+        srw = s_srw
+        indx = []
+        rg_3 = []
+        ms_reg = []
+        total_y_indexs = []
+        total_y_srws = []
+        tp=50
+        for k in range(len(srw)):
+            total_y_indexs.append([])
+            total_y_srws.append([])
+            indx.append([])
+            # indx[-1].append(0)
+            rg_3.append([])
+            rg_3[-1].append(df.iloc[k]['3_regime_index'])
+            for i in range(tp):
+                indx[-1].append(np.where(srw[k] == np.sort(srw[k])[::-1][:tp][i])[0][0])
+                rg_3[-1].append(df.iloc[indx[-1][-1]]['3_regime_index'])
+                
+            if rg_3[k][0] != 2:
+                same_reg = np.where(rg_3[k]==rg_3[k][0])[0]
+                turn_points = np.where(np.array(rg_3[k])==2)[0]
+                num_same_regime = len(same_reg)
+                num_turn_ponits = len(turn_points)
+                indx_same = np.array(indx[k])[list(same_reg[1:]-1)]
+                indx_2 = np.array(indx[k])[list(turn_points-1)]
+                total_y_indexs[-1].append(np.concatenate((indx_same,indx_2), axis=0))
+                srw[k][indx_2] *= num_turn_ponits
+                srw[k][indx_same] *= num_same_regime
+                ssr = srw[k][total_y_indexs[-1]]
+                
+            
+                total_y_srws[-1].append(ssr[0]/sum(ssr[0]))
+                
+            if rg_3[k][0] == 2:
+                same_reg = np.where(rg_3[k]==rg_3[k][0])[0]
+                other_points = np.where(np.array(rg_3[k])!=2)[0]
+                num_same_regime = len(same_reg)
+                num_turn_ponits = len(other_points)
+                indx_same = np.array(indx[k])[list(same_reg[1:]-1)]
+                indx_2 = np.array(indx[k])[list(other_points-1)]
+                total_y_indexs[-1].append(np.concatenate((indx_same,indx_2), axis=0))
+                
+                srw[k][indx_2] *= num_turn_ponits
+                srw[k][indx_same] *= num_same_regime 
+                
+                ssr = srw[k][total_y_indexs[-1]]
+                # ssr_0 , ssr_1 = np.unique(ssr[0], return_index=True)
+                # ssr = ssr_0[np.argsort(ssr_1)]
+                # ssr = ssr[ssr!=0]
+                # total_y_indexs = total_y_indexs[np.argsort(ssr_1)]
+                
+                total_y_srws[-1].append(ssr[0]/sum(ssr[0]))
+                
+                
+        start_point = 31
+        forcast_ts = np.zeros_like(time_serie)
+        forcast_ts[:start_point] = time_serie[:start_point]
+        for i in range(start_point-1,time_serie.shape[0]-1):
+            y = time_serie[i]+(time_serie[i]-time_serie[total_y_indexs[i][0]])/(i-total_y_indexs[i][0])
+            forcast_ts[i+1]=(total_y_srws[i]*y).sum()
+            
+        return forcast_ts
+    
+    def error_bars(self, selected_data, forcast_ts):
+        RMSE = math.sqrt(np.square(np.subtract(selected_data,forcast_ts)).mean())
+        NRMSE = RMSE/(max(selected_data)-min(selected_data))
+        MAPE=np.mean(np.abs((selected_data - forcast_ts)/selected_data))*100
+        MAP=np.mean(np.abs((selected_data - forcast_ts)))
+        return RMSE, NRMSE, MAPE, MAP
+    
+class VG_Modules_Model3(object):
     def __init__(self):
         self.bw = 1
     
@@ -620,6 +834,43 @@ class Net_moduls(object):
                     if sym_from != sym_to:
                         corr_coef = df_cc.loc[sym_from, sym_to]
                         if abs(corr_coef) > threshold:
+                            list_from.append(sym_from)
+                            list_to.append(sym_to) 
+                            list_corr_coeff.append(corr_coef)
+
+            # create df for constructing graph
+            df_graph = pd.DataFrame({'from':list_from, 'to':list_to, 
+                                    'corr coeff':list_corr_coeff})
+
+            G_crossed = nx.from_pandas_edgelist(df_graph, 'from', 'to')
+            return G_crossed
+        
+        if method == 'crossed_100_':
+            cross_offsets = []
+            for i in range(len(df.columns)):
+                for j in range(len(df.columns)):
+                    seconds = 5
+                    fps = 5
+                    rs = [self.crosscorr(df[df.columns[i]],df[df.columns[j]], lag) for lag in range(-int(seconds*fps),int(seconds*fps+1))]
+                    offset = np.floor(len(rs)/2)-np.argmax(rs)
+                    cross_offsets.append(offset)
+            cross_offsets = np.array(cross_offsets)
+            cross_offsets = cross_offsets/max(cross_offsets)
+            crossed_offset_matrix = cross_offsets.reshape((df.shape[1],df.shape[1]))
+            crossed_df = pd.DataFrame(crossed_offset_matrix , columns=(df.columns)).set_index(df.columns)
+            df_cc = crossed_df
+
+
+            # apply corr coeff threshold and create new df
+            list_symbols = df_cc.columns.to_list()
+            list_from = []
+            list_to = []
+            list_corr_coeff = []
+            for i , sym_from in enumerate(list_symbols):
+                for sym_to in list_symbols:
+                    if sym_from != sym_to:
+                        corr_coef = df_cc.loc[sym_from, sym_to]
+                        if abs(corr_coef) < threshold:
                             list_from.append(sym_from)
                             list_to.append(sym_to) 
                             list_corr_coeff.append(corr_coef)
